1e245106253e4cba18e7f9671481ec28a215d8ee
hanseyll <vanhanseyll@gmail.com>
2023-01-16 10:32:31 -0500

    Structuring project, postEntity done

---
+++ src/main/java/com/hansey/facebookclone/repository/PostEntityRepository.java
@@ -0,0 +1,9 @@
+package com.hansey.facebookclone.repository;
+
+import com.hansey.facebookclone.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostEntityRepository extends JpaRepository<PostEntity,String> {
+}
